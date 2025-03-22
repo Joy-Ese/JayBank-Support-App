054174d99942b9c04da4e36a0a2738aff124ea58
Joy-Ese <ihamajoyeseosa@gmail.com>
2025-03-22 16:50:52 +0000

    - successfully run migration. tables populated in the database

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
 import os
 
 DATABASE_URL = "postgresql://postgres:Joyeseosa@localhost:5432/JayBankDB"
