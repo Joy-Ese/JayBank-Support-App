d9b4e3a24a8515d37a5c8869cb16c01ad9a75f70
Joy-Ese <ihamajoyeseosa@gmail.com>
2025-04-12 18:12:27 +0100

    - personalized home page UI when a user is logged.

--- Jaybank.Client/src/app/pages/home/home.component.ts
+++ Jaybank.Client/src/app/pages/home/home.component.ts
@@ -1,24 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+interface UserDetails {
+  id: number;
+  first_name: string;
+  username: string;
+  email: string;
+  credits_remaining: number;
+  plan_subscribed_to: string;
+  role: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  featuredServices = [
-    {
-      title: 'AI Assistance',
-      description: 'Get instant support for your banking queries'
-    },
-    {
-      title: 'Credit System',
-      description: 'Flexible credits for seamless interactions'
-    },
-    {
-      title: 'Quick Responses',
-      description: 'Fast and accurate AI-powered resolutions'
+export class HomeComponent implements OnInit{
+  isAuthenticated: boolean = false;
+
+  token: string | null = null;
+
+  userDetails: UserDetails = {} as UserDetails;
+
+  constructor(
+    public authService: AuthService,
+  ) {}
+
+  ngOnInit(): void {
+    if (typeof window !== 'undefined' && localStorage) {
+      this.isAuthenticated = this.authService.isAuthenticated();
+      this.token = this.authService.getToken();
+
+      const userDetailsString = localStorage.getItem('userDetails');
+      if (userDetailsString) {
+        this.userDetails = JSON.parse(userDetailsString);
+      }
+    } else {
+      console.warn('localStorage is not available.');
     }
-  ];
+  }
+
+
+
 }
